53c08601ec230a99f56a5ea1708e32128036df5e
bhaskarsen98 <bhaskarsen1998@gmail.com>
2021-07-12 01:22:36 +0530

    Localization : add options to choose locale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'POC-crud-app';
+
+  locales = [
+    {code: 'en-US', label: 'English'},
+    {code: 'hi', label: 'हिंदी'},
+  ]
+
+  constructor(@Inject(LOCALE_ID) protected localeId: string){}
 }
--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { employee } from 'src/app/domain/employee';
 import { EmployeeService } from 'src/app/services/employee.service';
-import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-table',
@@ -12,14 +13,15 @@ export class TableComponent implements OnInit {
 
   temp: employee[] = [];
   isBusy: boolean = false;  //when dialog is open
-
-  // edit-employee
+  useCase: string = ''; //for opening add/edit dialog
+  
+  // add-edit-employee dialog
   employeeDialog: boolean;
   submitted: boolean; //invalid/empty field
   tempEmp: employee;
   id: string;
 
-  constructor(public empService: EmployeeService) { }
+  constructor(private httpc: HttpClient, public empService: EmployeeService) { }
 
   ngOnInit(): void { 
     //loads table using get req
@@ -38,31 +40,68 @@ export class TableComponent implements OnInit {
     
   };
 
-  //edit-employee
+  //add-edit-employee
+  //open dialog and initialize the fields
   openNew(CurrId: string) { 
     console.log("inOpen");
-    this.id = CurrId;
-    this.empService.getById(this.id).subscribe((data: employee) => {  //prepopulate data
-      this.tempEmp = data;
-    });
+    if(CurrId != '') {
+      this.useCase = 'Edit';
+      this.id = CurrId;
+      this.empService.getById(this.id).subscribe((data: employee) => {  //prepopulate data
+        this.tempEmp = data;
+      });
+    } else {
+      this.useCase = 'Add';
+      this.tempEmp = {
+        empName: '',
+        jobTitle: '',
+        status: 'false'
+      };
+    }
     this.submitted = false;
     this.employeeDialog = true;
   }
 
+  //hide dialog using cancel button
   hideDialog() {
     this.employeeDialog = false;
     this.submitted = false;
   }
+
+  //save changes based useCase 
+  saveChanges() {
+    if(this.useCase == 'Edit'){
+      this.editEmployee();
+    } else if(this.useCase == 'Add'){
+      this.saveEmployee();
+    } else {
+
+    }
+
+  }
   editEmployee() {
     this.submitted = true;
-    // console.log(this.tempEmp);
     if(this.tempEmp.empName != '' && this.tempEmp.jobTitle != ''){
       this.empService.update(this.id, this.tempEmp).subscribe(() => {
-        // console.log("employeeUpdated");
         this.ngOnInit();  //reload table
         this.hideDialog();
         
       });
+    }
+  }
+
+  saveEmployee() {
+    this.submitted = true;
+    if(this.tempEmp.empName != '' && this.tempEmp.jobTitle != ''){
+      this.empService.create(this.tempEmp).subscribe(res =>{ 
+        console.log('employee added!');
+        this.employeeDialog = false;
+        this.submitted = true;
+        // location.reload(true); //wrong way , use routes and shift button to table component. Call ngOnINit hook there(get call to reload table)
+        this.ngOnInit();  //reload table
+        this.hideDialog();
+
+      })
     }
   }
 
