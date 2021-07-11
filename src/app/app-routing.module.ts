b5ecb1a32112b8fd1adfb1b854c72db2e7b53593
bhaskarsen98 <bhaskarsen1998@gmail.com>
2021-07-12 01:16:12 +0530

    remove routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,16 @@ import { TableComponent } from './component/table/table.component';
 import { RemEmployeeComponent } from './component/rem-employee/rem-employee.component';
 
 const routes: Routes = [
-  { path: 'POC', redirectTo: 'POC/table', pathMatch: 'full'},
-  { path: 'POC/table', component: TableComponent },
-  { path: 'POC/add', component: AddEmployeeComponent },
-  { 
-    path: 'POC/:id', component: EditEmployeeComponent,
-    children: [
-      { path: '', redirectTo: 'edit', pathMatch: 'full' },
-      { path: 'edit', component: EditEmployeeComponent },
-      ],
-  } ,
+  // { path: '', redirectTo: 'table', pathMatch: 'full'},
+  // { path: 'table', component: TableComponent },
+  // { path: 'table/add', component: AddEmployeeComponent },
+  // { 
+  //   path: 'table/edit/:id', component: EditEmployeeComponent,
+  //   // children: [
+  //   //   { path: '', redirectTo: 'edit', pathMatch: 'full' },
+  //   //   { path: 'edit', component: EditEmployeeComponent },
+  //   //   ],
+  // } ,
 ];
 
 @NgModule({
